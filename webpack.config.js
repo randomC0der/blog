2ff47072d9485f6c0a46c58ab1383c3e0f735515
Lukas Hein <lukashein@protonmail.com>
2022-01-02 22:02:39 +0100

    add normalize.css from npm

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.env.APP_ENV === 'development';
+
+const baseFilename = !isDev ? '[name]' : '[name].[contenthash]';
+
+module.exports = {
+  entry: {
+    normalize: "normalize.css", //path.resolve(__dirname, 'src', 'assets', 'js', 'index.js'),
+    // path.resolve(__dirname, 'src', 'assets', 'css', 'index.css'),
+  },
+  output: {
+    path: path.resolve(__dirname, "_site", "assets", "js"),
+    filename: `${baseFilename}.js`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: path.join("..", 'css', `${baseFilename}.css`) })],
+};
