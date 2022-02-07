8b522c749493f187074f8c22450a356b7a511c37
Lukas Hein <lukashein@protonmail.com>
2022-02-07 15:59:19 +0100

    Add 11ty plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('CNAME');
+  eleventyConfig.addPassthroughCopy("src/assets/static/");
+
   return {
     dir: { input: "src", output: "_site" },
   };
